8c6ba77a58aed5346d8ef5edf417e3a14dcb847b
AnastasiaMitrea <anastasia.mitrea@gmail.com>
2025-05-12 14:34:54 +0300

    am creat definitia clasei pasager

--- includes/Pasager.h
+++ includes/Pasager.h
@@ -4,15 +4,38 @@
 #include <iostream> 
 #include <string>
 #include <vector>
+
 #include "Persoana.h"
 #include "Bilet.h"
 #include "Zbor.h"
 
-using namespace std;
+namespace Transport_Aerian {
 
-class Pasager : public Persoana {
+    class Pasager : public Persoana {
+
+    public:
+        Pasager(); 
+        Pasager(const std::string& nume, const std::string& prenuume, const std::string& cnp, int varsta, const Bilet& bilet);   
+        Pasager(const Pasager& other);
+        ~Pasager();
+
+        void AfisareData() const override;  //suprascriu metoda virtuala pura din clasa de baza
+        void AdaugaZbor(Zbor* zbor); //adaug un pointer la Zbor in lista de zboruri ale pasagerului
+        static int GetNumarPasageri(); //functia statica care returneaza cati pasageri au fost creati
+        const Bilet& GetBilet() const;
+        const std::vector<Zbor*>& GetZboruri() const; //getter pt lista de zboruri
+
+        Pasager operator+(Zbor* zbor) const; //supraincarcare operator + pt a adauga un zbor
+
+        friend std::ostream& operator<<(std::ostream& out, const Pasager& pasager);
+
     private:
-        Bilet pasagerBilet; //obiect de tip Bilet asociat pasagerului
-        Zbor pasagerZbor; //obiect de tip Zbor asociat pasagerului
+        Bilet m_Bilet; //obiect de tip bilet asociat pasagerului
+        std::vector<Zbor*> m_zboruri;  //lsita de zboruri ale unui pasager - obiecte pe heap, gestionate dinamic
+        static int m_numar_pasageri; // membru static care retine numarul total de pasageri creati
+        void _IncrementareContor(); //metoda privata pt a creste numarul de pasageri
+    };
 
-};
+}
+
+#endif PASAGER_H
