0af66de6a8d5e7affe8b1bc0ad6d1ae046763128
AnastasiaMitrea <anastasia.mitrea@gmail.com>
2025-05-26 14:44:16 +0300

    Clasa Retea pt a stoca distantele dintre orase pt zboruri

---
+++ includes/Retea_zboruri.h
@@ -0,0 +1,29 @@
+#ifndef RETEA_H
+#define RETEA_H
+
+#include <string>
+#include <map>
+
+namespace Transport_Aerian {
+
+    class Retea_Rute {
+    public:
+        Retea_Rute();
+
+        void AdaugaDistanta(const std::string& oras1, const std::string& oras2, int distanta_km);  //distanta dintre 2 orase
+        int GetDistanta(const std::string& oras1, const std::string& oras2) const;
+        int CalculeazaPretBaza(const std::string& oras1, const std::string& oras2) const;  //pret in functie de distanta
+
+    private:
+        std::map<std::pair<std::string, std::string>, int> m_distante;
+        //dictionar cheie - valoare cu cheie = pereche string uri ("oras1" , "oras2") si valoare = distanta (oras1, oras2)
+        // ex: ("Bucuresti", "Paris"), 1800
+
+        std::pair<std::string, std::string> _NormalizareCheie(const std::string& oras1, const std::string& oras2) const;
+        //Normalizare = ordonez cheile din dictionar astfel incat ("Bucuresti", "Paris") sa fie acelasi cu ( "Paris", "Bucuresti")
+        //este folosita intern in Retea, nu va afecta m_plecare si m_sosire la znboruri
+    };
+
+}
+
+#endif
---
+++ src/Retea_zboruri.cpp
@@ -0,0 +1,33 @@
+#include "../includes/Retea_zboruri.h"
+#include <algorithm>
+
+using namespace Transport_Aerian;
+
+Retea_Rute::Retea_Rute() {}
+
+void Retea_Rute::AdaugaDistanta(const std::string& oras1, const std::string& oras2, int distanta_km) {
+    auto key = _NormalizareCheie(oras1, oras2);
+    m_distante[key] = distanta_km;  //adaug distanta in dictionar cu cheia ordonata
+}
+
+int Retea_Rute::GetDistanta(const std::string& oras1, const std::string& oras2) const { 
+    auto key = _NormalizareCheie(oras1, oras2); 
+    auto it = m_distante.find(key); //caut cheia key (pereche de 2 orase) in dictionar cu iterator
+    //.find() returneaza un iterator catre cheia gasita in dictionar iar daca iteratorul este .end() inseamna ca nu a fost gasita cheia
+    if(it != m_distante.end())   
+        return it->second;   //second = distanta si first=(oras1, oras2) din dictionar
+    return -1; // nu exista ruta in dictionar => distanta = -1
+}
+
+int Retea_Rute::CalculeazaPretBaza(const std::string& oras1, const std::string& oras2) const {
+    int distanta = GetDistanta(oras1, oras2);
+    if(distanta == -1) 
+        return -1;
+    return distanta / 10; // ex: 1800 km = 180 EUR (1 EUR = 10 km)
+}
+
+std::pair<std::string, std::string> Retea_Rute::_NormalizareCheie(const std::string& oras1, const std::string& oras2) const {
+    if(oras1 < oras2)   //compar alfabetic pt a decide care oras vine primul in pereche
+        return {oras1, oras2};
+    return {oras2, oras1}; 
+}
