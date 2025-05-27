33ae841c672edeefaae4aa2797dd074dfe4d802b
AnastasiaMitrea <anastasia.mitrea@gmail.com>
2025-05-28 01:08:07 +0300

    Am adaugat un membru Companie* in clasa Rezervare

--- includes/Rezervare.h
+++ includes/Rezervare.h
@@ -5,12 +5,13 @@
 #include "Zbor.h"
 #include "Pilot.h"
 #include "I_Afisabil.h"
+#include "Companie.h"
 
 namespace Transport_Aerian {
 
     class Rezervare : public Interfete::IAfisabil {
     public:
-        Rezervare(const Pasager& pasager, Zbor* zbor, const std::vector<Pilot*>& echipaj); 
+        Rezervare(const Pasager& pasager, Zbor* zbor, const std::vector<Pilot*>& echipaj, Companie* companie); 
         virtual ~Rezervare();
 
         void Afisare() const;
@@ -19,11 +20,14 @@ namespace Transport_Aerian {
         const Pasager& GetPasager() const;
         Zbor* GetZbor() const;
         const std::vector<Pilot*>& GetEchipaj() const;
+        Companie* GetCompanie() const;
+
 
     private:
         Pasager m_pasager;
         Zbor* m_zbor;
         std::vector<Pilot*> m_echipaj;
+        Companie* m_companie;
     };
 
 }
--- src/Rezervare.cpp
+++ src/Rezervare.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 using namespace Transport_Aerian;
 
-Rezervare::Rezervare(const Pasager& pasager, Zbor* zbor, const std::vector<Pilot*>& echipaj)
-    : m_pasager(pasager), m_zbor(zbor), m_echipaj(echipaj) {}
+Rezervare::Rezervare(const Pasager& pasager, Zbor* zbor, const std::vector<Pilot*>& echipaj, Companie* companie)
+    : m_pasager(pasager), m_zbor(zbor), m_echipaj(echipaj), m_companie(companie) {}
 
 Rezervare::~Rezervare() {}
 
@@ -19,15 +19,18 @@ void Rezervare::Afisare() const {
 
     cout << "Echipaj piloti:" << endl;   //afisez pilotii asignati zborului
     for (const auto& p : m_echipaj)
-        if (p) p->Afisare();
+        if (p) 
+            p->Afisare();
 
-    cout << "Va multumim pentru ca ati ales sa zburati cu noi. Calatorie placuta! --- " << m_zbor.GetCompanie() << endl;
+    if (m_companie)
+        cout << "Va multumim pentru ca ati ales sa zburati cu noi. " << m_companie->GetNume() << " va doreste o calatorie placuta!" << endl;
     cout << "===================" << endl;
 }
 
 std::string Rezervare::DescriereText() const {
     std::string text = m_pasager.DescriereText() + "\n";
     text += m_pasager.GetBilet().DescriereText() + "\n";
+
     if (m_zbor)
         text += "Ruta: " + m_zbor->GetPlecare() + " → " + m_zbor->GetDestinatie() + "\n";
 
@@ -36,17 +39,21 @@ std::string Rezervare::DescriereText() const {
         if (p)
             text += " - " + p->GetNume() + " " + p->GetPrenume() + "\n";
 
+    if (m_companie)
+        text += "Companie: " + m_companie->GetNume() + "\n";
+
     return text;
 }
 
-const Pasager& Rezervare::GetPasager() const {
-    return m_pasager;
+const Pasager& Rezervare::GetPasager() const { 
+    return m_pasager; 
 }
-
-Zbor* Rezervare::GetZbor() const {
-    return m_zbor;
+Zbor* Rezervare::GetZbor() const { 
+    return m_zbor; 
 }
-
-const std::vector<Pilot*>& Rezervare::GetEchipaj() const {
-    return m_echipaj;
+const std::vector<Pilot*>& Rezervare::GetEchipaj() const { 
+    return m_echipaj; 
+}
+Companie* Rezervare::GetCompanie() const { 
+    return m_companie; 
 }
