839a43380d1cb7f03cb45e001642278caa099679
AnastasiaMitrea <anastasia.mitrea@gmail.com>
2025-05-29 00:25:34 +0300

    Functie in utils

--- includes/Utils.h
+++ includes/Utils.h
@@ -5,6 +5,13 @@ namespace Utilitati{
 
     inline int CharToInt(char c) { return c - '0'; }  //functie facuta pt claritate in cod, pt a nu scrie " - '0' " la fiecare conversie char - int
 
+    bool ContineCifre(const std::string& text) {
+        for (char c : text)
+            if (isdigit(c))
+                return true;
+        return false;
+    }
+
 }
 
 #endif
