009d1f52c97d50375a97f66ea46a70c74770390f
AnastasiaMitrea <anastasia.mitrea@gmail.com>
2025-05-29 00:26:02 +0300

    Functie adaugata in Date

--- includes/Date.h
+++ includes/Date.h
@@ -13,6 +13,8 @@ namespace Utilitati {
         Date() : zi(0), luna(0), an(0) {} //constructor default
         Date(int zi, int luna, int an) : zi(zi), luna(luna), an(an) {}  //constructor cu lista de initializare
 
+        int GetZi() const { return zi;}
+
         //afisare data in formatul DD/MM/YYYY
         friend std::ostream& operator<<(std::ostream& os, const Date& date) {
             os << date.zi << "/" << date.luna << "/" << date.an;
@@ -20,6 +22,6 @@ namespace Utilitati {
         }
     };
 
-}
+};
 
 #endif
